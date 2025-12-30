b5eb78c71c2cefcf87f17ce891a933c2ab072965
strjak <strackjuanp@gmail.com>
2025-12-30 04:02:26 +0000

    Add solution to exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,6 +1,16 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = "";
+    for (let i = str.length - 1; i >= 0; i--){
+        newStr += str[i];
+    }
+    return newStr;
 };
+
+// another aproach
+
+// const reverseString = function(str){
+//     return str.split("").reverse().join("");
+// }
 
 // Do not edit below this line
 module.exports = reverseString;
