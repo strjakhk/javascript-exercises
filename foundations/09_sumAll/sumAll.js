4aab1c421181192e355e1aaac7c13e77d2bf0c9f
strjak <strackjuanp@gmail.com>
2025-12-30 04:56:12 +0000

    Add solution to exercise 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
+const sumAll = function(...range) {
+    range = range.sort((a, b) => a - b);
 
+    const a = range[0];
+    const b = range[1];
+    
+    if (typeof a == "string" || typeof b == "string") return "ERROR";
+    if (a % 1 != 0 || b % 1 != 0) return "ERROR";
+    if (a < 0 || b < 0) return "ERROR";
+
+    let sum = 0;
+    for (let i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
