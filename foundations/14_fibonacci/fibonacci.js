00499f27d698aa3337a1c4ba10d2a0021852dc6b
strjak <strackjuanp@gmail.com>
2026-01-14 03:13:18 +0000

    Complete fibonacci exercise

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(n) {
+    if (n < 0) return "OOPS"
+    let prev = 0;
+    let curr = 1;
+    for (let i = 0; i < n; i++){
+        let oldCurr = curr;
+        curr += prev;
+        prev = oldCurr;
+    }
+    return prev;
 };
 
 // Do not edit below this line
