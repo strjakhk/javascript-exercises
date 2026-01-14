9ec8b599cb1ca9c294e4b4bc776f2897babfd30f
strjak <strackjuanp@gmail.com>
2026-01-14 02:32:32 +0000

    Add solution to exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    /**
+     * transform str to Uppercase
+     * get only letters from string
+     * create a new reverse copy of the string
+     * compare the two copies
+     */
 
+    const strLetters = [...str.toUpperCase()].filter(letter => {
+        const code = letter.charCodeAt();
+        return (code >= 65 && code <= 90)
+                || (code >= 48 && code <= 57);
+    });
+    return strLetters.join("") == strLetters.reverse().join("");
 };
 
 // Do not edit below this line
