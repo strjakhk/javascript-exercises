4e50e9657e933fcaad0cdb777d4da9b4ab588b31
strjak <strackjuanp@gmail.com>
2026-01-14 03:16:52 +0000

    Complete exercise 15

--- foundations/15_getTheTitles/getTheTitles.js
+++ foundations/15_getTheTitles/getTheTitles.js
@@ -1,5 +1,17 @@
-const getTheTitles = function() {
+const books = [
+  {
+    title: 'Book',
+    author: 'Name'
+  },
+  {
+    title: 'Book2',
+    author: 'Name2'
+  }
+]
 
+const getTheTitles = function(title) {
+    const titles = books.map(book => book.title);
+    return titles;
 };
 
 // Do not edit below this line
